c71cb2f22d1426ca9a0ae09f7f829f217650a98a
shruti <shrutigupta5555@gmail.com>
2021-09-25 15:03:18 +0530

    add: auth

---
+++ firebase.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDMNcpRYbrBpD4FqpkTcFxJSeaXJSxzeEE",
+  
+    authDomain: "expresso-4d221.firebaseapp.com",
+  
+    projectId: "expresso-4d221",
+  
+    storageBucket: "expresso-4d221.appspot.com",
+  
+    messagingSenderId: "604021317026",
+  
+    appId: "1:604021317026:web:b35ff513da7c086d4a66fb"
+  
+  };
+  
